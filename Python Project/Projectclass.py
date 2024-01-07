53a257ec3705a918315f01c348ef0dbf04977479
T-Poison10 <dipansilwal10@gmail.com>
2024-01-07 21:05:23 +0545

    balanced code

--- Python Project/Projectclass.py
+++ Python Project/Projectclass.py
@@ -1,45 +1,26 @@
-from PyPDF2 import PdfReader
 from PyPDF2 import PdfWriter
 import tkinter as tk
 from tkinter import filedialog
-
 merger = PdfWriter()
-
-root = tk.Tk()
-class OpenDir:
-    def __init__(self) -> None:
-        self.pdf_number = int(input("Enter the no of pdf's you want to merge:")) 
-        self.pdf_lst = [] 
-
-    def opendir(self,cls):
-        self.path = filedialog.askopenfilename(title="Select a File", filetypes=[("PDF Files", "*.pdf"), ("All files", "*.*")])
-        self.pdf_lst.append(self.path)
-
-
-
-    def read_pages(self,cls):
-        for self.pdf_path in self.pdf_lst:
-            self.pdfreader = PdfReader(self.pdf_path)
-            self.totalpages = len(self.pdfreader.pages)
-            print(self.totalpages)
-
-    def merge_pdf(self):
-        for self.pdf_path in self.pdf_lst:
-            merger.append(self.pdf_path)
-
-        with open("Merged_File.pdf","wb") as pdf:
-            merger.write(pdf) 
-
-    def run(self):
-        self.i = 0
-        for self.i in range(0,self.pdf_number):
-            self.opendir(self)
-
-        self.read_pages(self)
+class merge():
+    def __init__(self) :
+        self.pdfCount = int(input("Enter how many pdfs you want to merge:"))
+        self.pdf_list = []
+    def appendPath(self):
+        self.path = filedialog.askopenfilename(title = "Choose your file" , filetypes=[("PDF Files",".pdf"),("All files","*.*")])
+        self.pdf_list.append(self.path)
+    def mergePdf(self):
+        for self.path in self.pdf_list:
+            merger.append(self.path)
+        self.finalpath = filedialog.askdirectory(title = "Choose where you want to save your file")
+        with open (f"{self.finalpath}//Merged_Pdf.pdf","wb") as Mergedpdf:
+            merger.write(Mergedpdf)
+    def RunMerger(self):
+        for self. i in range(0,self.pdfCount):
+            self.appendPath()
+        self.mergePdf()
 
 if __name__ == "__main__":
-    file = OpenDir()
-    file.run()
-    file.merge_pdf()
-
-
+    PdfMerger = merge()
+    PdfMerger.RunMerger()
+        
